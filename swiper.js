5c013e0353938bcccc09dd060a0833f4c2d4e409
kraubunas <kraubun@gmail.com>
2022-02-01 16:27:25 +0200

    updated with more sections, fixed responsivness in awesome section and about section

---
+++ swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+});
